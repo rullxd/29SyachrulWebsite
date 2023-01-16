29df7701c2df5493bb82952c0c222fb238eb7766
rullxd <122759599+rullxd@users.noreply.github.com>
2023-01-16 08:54:42 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta carshet="UTF-8">
+    <meta http-equiv="X-UA-Compoatible" content="IE-edge">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title> </title>
+</head>
+
+<body>
